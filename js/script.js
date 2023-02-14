f0cc539115590539483d95d2b584db3651c6ac4d
Ivan Hamou <ivanhamou@gmail.com>
2023-02-14 11:03:24 +0100

    finished up with js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,47 @@
 const hamburger = document.querySelector(".hamburger")
-const nav = document.querySelector("aside")
-console.log(nav);
+const navAside = document.querySelector("aside")
+const navAsideItem = document.querySelectorAll("aside a")
+const main = document.querySelector("main")
 
 hamburger.addEventListener("click", () => {
-    nav.classList.toggle("displayMenu");
+    navAside.classList.toggle("displayMenu");
+    hamburger.classList.toggle("rotateBurger")
 })
+
+main.addEventListener("click", () => {
+    navAside.classList.remove("displayMenu");
+    hamburger.classList.remove("rotateBurger")
+})
+
+navAsideItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        navAside.classList.remove("displayMenu");
+        hamburger.classList.remove("rotateBurger")
+    })
+})
+
+
+
+
+const sections = document.querySelectorAll(".scroll");
+const navLi = document.querySelectorAll("nav a");
+
+
+window.addEventListener("scroll", () => {
+    let current = "homeAnchor"
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop -200) {
+            current = section.getAttribute("id")
+        }    
+    });
+
+    navLi.forEach((a) => {
+        a.classList.remove("active")
+        if (a.classList.contains(current)) {
+            a.classList.add("active")
+        }
+    });
+})
+        
+
